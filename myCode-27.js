826471cdb96297736d5f68163d9d9e81f4711a0e
Demelena <demelena@gmail.com>
2018-11-13 23:40:53 -0800

    Advanced Modifying Images-2

--- myCode-27.js
+++ myCode-27.js
@@ -1,4 +1,5 @@
 // write your code here
+//The green screen algorithm
 var fgImage = new SimpleImage("drewRobert.png");
 var bgImage = new SimpleImage("dinos.png");
 var output = new SimpleImage(fgImage.getWidth(), fgImage.getHeight());
@@ -18,6 +19,80 @@ for (var pixel of fgImage.values()){
 print (fgImage);
 print (bgImage);
 print (output);
+
+//  A program that draws a square 200 pixels by 200 pixels and that looks like this square with colors red (red value 255), green (green value 255), blue (blue value 255) and magenta (red value 255 and blue value 255). All other RGB values are set to 0.
+var img = new SimpleImage(200,200);
+for (var px of img.values()){
+  var x = px.getX();
+  var y = px.getY();
+  if (x < img.getWidth()/2){
+    px.setRed(255);
+  }
+  if (y>img.getHeight()/2){
+    px.setBlue(255);
+  }
+  if (x > img.getWidth()/2 && y<img.getHeight()/2)
+    {px.setGreen(255);
+  }
+}
+print (img);
+
+// Returning pixel with its red, green, and blue components changed so that the pixel’s color is black.
+var image = new SimpleImage("usain.jpg");
+for ( var pixel of image.values()) {
+        pixel.setRed(0);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+    }
+print(image);
+
+// A function named setBlack that has one parameter pixel (representing a single pixel) and returns pixel with its red, green, and blue components changed so that the pixel’s color is black.
+
+function setBlack(name)
+{
+var image = new SimpleImage(name);
+for ( var pixel of image.values()) {
+        pixel.setRed(0);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+    }
+print(image);
+}
+setBlack("usain.jpg");
+
+
+// Adding a black border to an image
+var img = new SimpleImage("hilton.jpg");
+for ( var pixel of img.values())
+{
+  var x = pixel.getX();
+  var y = pixel.getY();
+    if (x <= 10 || y<= 10 || x >= img.getWidth() - 10 || y>= img.getHeight() - 10)
+    {
+        pixel.setRed(0);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+    }
+}
+print (img);
+
+// A function named addBorder. This function will add a black border to an image
+function addBorder (imageWithBorder, thicknessOfBorder){
+var image = new SimpleImage(imageWithBorder);
+for ( var pixel of image.values())
+{
+  var x = pixel.getX();
+  var y = pixel.getY();
+    if (x <= thicknessOfBorder || y<= thicknessOfBorder || x >= image.getWidth() - thicknessOfBorder || y>= image.getHeight() - thicknessOfBorder)
+    {
+        pixel.setRed(0);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+    }
+}
+print(image);
+}
+addBorder ("rodger.png", 20)
 
 
 
